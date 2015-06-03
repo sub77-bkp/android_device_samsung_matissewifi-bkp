06187e09e50e873b9f324054e660c9678e005259
sub77-bkp <sub77@ymail.com>
2015-06-03 13:30:40 +0200

    Build libsecril from Source
    Change-Id: Iee44a3e8aa045045a53e09723c942a42682025cc

--- device.mk
+++ device.mk
@@ -58,5 +58,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
 
+PRODUCT_PACKAGES += \
+    libsecril-client \
+    libsecril-client-sap
+
 # Inherit from qcom-common
 $(call inherit-product, device/samsung/msm8226-common/msm8226.mk)
